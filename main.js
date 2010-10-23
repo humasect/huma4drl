51ad1c635124e79b0afcd87b22d7d5ebfaec7522
Lyndon Tremblay <humasect@gmail.com>
2010-10-23 05:58:13 -0600

    add jquery and underscore

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function createMap() {
-    return 4;
+var TILEWIDTH = 24;
+var TILEHEIGHT = 30;
+var MAPWIDTH = 32;
+var MAPHEIGHT = 24;
+function forEachTileXy(Fun) {
+    for (var i = 0; i <= MAPWIDTH; i += 1) {
+        for (var j = 0; j <= MAPHEIGHT; j += 1) {
+            Fun(i, j);
+        };
+        null;
+    };
+    return null;
 };
+function createMap() {
+    return forEachTileXy(function (x, y) {
+        return $('#view').append('<div></div>');
+    });
+};
+$(document).ready.lambda(null, createMap());
